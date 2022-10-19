5d81e6edef19d379d21bb6ee360a07bfaeab840a
thikeyeman <86958541+thikeyeman@users.noreply.github.com>
2022-10-19 14:53:47 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 enchant();
+var screenWidth = 1000;
+var screenHeight = 1000;
+
+window.onload = function () {
+    var game = new Core(screenWidth, screenHeight);
+    game.fps = 30;
+
+    var userImg = "user.png";
+
+    game.preload(userImg);
+	
+	//user image
+    var userSprite = Class.create(Sprite, {
+        initialize: function (x, y) {
+            Sprite.call(this, 95, 83);
+            this.image = game.assets[userImg];
+            this.x = x;
+            this.y = y;
+        }
+    })
+	
+    var scoreScene = function () {
+        var scene = new Scene();
+		var user = new userSprite(10,10);
+		scene.addChild(user);
+        return scene;
+    };
+    game.onload = function () {
+        game.replaceScene(scoreScreen());
+    }
+    game.start();
+}
