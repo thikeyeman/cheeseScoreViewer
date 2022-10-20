210e1234b0f3386535a0ddd715e686403ba6af0e
Moehtetyan2 <mhtetyan556@gmail.com>
2022-10-20 09:54:51 +0900

    t

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 enchant();
+
+var screenWidth = 1000;
+var screenHeight = 1000;
+window.onload = function(){
+	var game = new Core(screenWidth,screenHeight);
+	game.fps = 30;
+	
+	var userImg = "../Assets/Illustrator Files/user.svg"
+	
+	game.preload(userImg);
+	
+	game.onload = function(){
+		
+	}
+	game.start;
+}
