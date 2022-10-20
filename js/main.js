8e81fb22e599c24f4391ac9b9f05a18a42e9769b
thikeyeman <86958541+thikeyeman@users.noreply.github.com>
2022-10-20 13:50:12 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -82,7 +82,7 @@ window.onload = function () {
 			this.y = y;
 			this.color ="Red";
 			this.size = 30;
-			
+			this.font = "bold 26px sans-serif";
 			
 		}
 	})
@@ -131,7 +131,7 @@ window.onload = function () {
 
 
 		for (i = 0; i < numOfPlayers; i++) {
-           usersNames[i] = new labelClass(100+10, offsetY+7.2-20 + i * 100);
+           usersNames[i] = new labelClass(100+10, offsetY+7.2-25 + i * 100);
             scene.addChild(usersNames[i]);
         }
 
@@ -140,7 +140,7 @@ window.onload = function () {
 		
 		///////////////////////////userAreaBackground Creation ** Begins///////////////////////////////////////////////////
         for (i = 0; i < numOfPlayers; i++) {
-            userAreaBgSprite[i] = new userAreaBgSprite(0, offsetY+7.2-35 + i * 100, i<5?i:1+i);
+            userAreaBgSprite[i] = new userAreaBgSprite(0, offsetY+7.2-45 + i * 100, i<5?i:1+i);
             scene.insertBefore(userAreaBgSprite[i],scene.firstChild);
         }
         ///////////////////////////userAreaBackground Creation ** Ends////////////////////////////////////////////////////
