6080affebbaebdc9d7651ec054893d54174d3f45
Sainjargal Bolortsetseg <azure.blr24@gmail.com>
2022-11-30 10:26:46 +0900

    Add files via upload

---
+++ gproject/script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  var popup = document.getElementById("ar-text");
+  var popup = document.getElementById("vr-text");
+  var popup = document.getElementById("vtuber-text");
+  var popup = document.getElementById("game-text");
+  popup.classList.toggle("show");
+}
